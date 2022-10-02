04445ea22d0cb8d2222db0fa1fba47b8fc49b29b
Viiktori <112752380+Viiktori@users.noreply.github.com>
2022-10-02 16:43:39 +0300

    Add files via upload

---
+++ java_HomeWork/src/main/java/lesson_1/CanSwim.java
@@ -0,0 +1,5 @@
+package lesson_1;
+
+public interface CanSwim {
+    double swim(Pool pool);
+}
---
+++ java_HomeWork/src/main/java/lesson_1/Utils.java
@@ -0,0 +1,7 @@
+package lesson_1;
+
+public class Utils {
+    public static void makeAnimalOlder(Animal animal) {
+        animal.setAge(animal.getAge() + 1);
+    }
+}

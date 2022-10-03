542c307f07fffd16da31bbbaac7dd9104812426a
Viiktori <112752380+Viiktori@users.noreply.github.com>
2022-10-03 21:17:49 +0300

    Delete java_HomeWork directory

--- java_HomeWork/src/main/java/lesson_1/CanSwim.java
+++
@@ -1,5 +0,0 @@
-package lesson_1;
-
-public interface CanSwim {
-    double swim(Pool pool);
-}
--- java_HomeWork/src/main/java/lesson_1/Utils.java
+++
@@ -1,7 +0,0 @@
-package lesson_1;
-
-public class Utils {
-    public static void makeAnimalOlder(Animal animal) {
-        animal.setAge(animal.getAge() + 1);
-    }
-}
